738a57505f7cacc0a825ad32c011b2e0cb45c47d
rinkudas <rinkudas.id@gmail.com>
2025-07-29 20:41:14 +0530

    test: returns 0 for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'StringCalculator' do
   before(:each) { StringCalculator.reset_call_count! }
 
   describe '.add' do
+    it 'returns 0 for empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
     # TEST_PLACEHOLDER
   end
 end
