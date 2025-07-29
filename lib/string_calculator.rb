d0be40d8b2e56aaf004becb6c2d4c993a5db6b2d
rinkudas <rinkudas.id@gmail.com>
2025-07-29 21:40:49 +0530

    feat: supports custom single-character delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -7,7 +7,16 @@ class StringCalculator
     def add(input)
       @call_count += 1
       return 0 if input.strip.empty?
-      return input.split(/,|\n/).map(&:to_i).sum
+      return input.to_i if input.strip.length == 1
+      delimiters = [",", "\n"]
+      if input.start_with?("//")
+        header, input = input.split("\n", 2)
+        custom = header.scan(/\[(.+?)\]/).flatten
+        custom = [header[2]] if custom.empty?
+        delimiters.concat(custom)
+      end
+      numbers = input.split(Regexp.union(delimiters)).map(&:to_i)
+      return numbers.sum
       # METHOD_PLACEHOLDER
     end
 
